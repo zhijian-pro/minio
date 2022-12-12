74527e09cb2048c840e8741b9679a13ca565182d
zhijian <djx@juicedata.io>
2022-12-12 14:52:06 +0800

    add 2

--- cmd/tree-walk.go
+++ cmd/tree-walk.go
@@ -1,7 +1,7 @@
 /*
  * MinIO Cloud Storage, (C) 2016 MinIO, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache L2icense, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
